eb3e50b57fe687ac81b628bacb41a890219ea827
wangguodong577 <290639027@qq.com>
2016-04-29 19:21:58 +0800

    t

--- router/index.js
+++ router/index.js
@@ -26,10 +26,23 @@ exports.init = function (app) {
 		});
 	});
 
-
-	app.get('/wechat/code',function(req,res){
-		https.get('https://open.weixin.qq.com/connect/oauth2/authorize', function(req,res) {
-		})
+	app.get('/wechat/userInfo/accessToken',function(req,res){
+		var url = req.body.url; //获取当前url
+		var arg = url.parse(req.url, true).query;
+		console.log(JSON.stringify(arg));
+		//signature.sign(url,function(signatureMap){
+		//	signatureMap.appId = wechat_cfg.appid;
+		//	// res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8'});
+		//	res.header("Access-Control-Allow-Origin", "*");
+		//	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		//	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+		//	res.header("X-Powered-By", ' 3.2.1');
+		//	res.header("Content-Type", "application/json;charset=utf-8");
+		//	res.end(JSON.stringify(signatureMap));
+		//	// res.send(signatureMap);
+		//	// res.render('index',signatureMap);
+		//	// console.log(signatureMap)
+		//});
 	});
 
     app.post('/wechat',function(req,res){
